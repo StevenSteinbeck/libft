e5e54ffb3517623f8a174cfdb1b68b4164c6a3ff
StevenSteinbeck <stevensteinbeck@yahoo.com>
2018-06-06 13:09:16 -0700

    Add files via upload

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -1,103 +1,88 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   austin_strsplit.c                                  :+:      :+:    :+:   */
+/*   ft_strsplit.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: stestein <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/04/28 17:07:24 by stestein          #+#    #+#             */
-/*   Updated: 2018/05/03 16:37:35 by stestein         ###   ########.fr       */
+/*   Created: 2018/02/28 08:58:50 by stestein          #+#    #+#             */
+/*   Updated: 2018/03/13 10:38:27 by stestein         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int		word_count(char const *s, char c)
+static int			cnt(char const *s, char c)
 {
-	int i;
-	int ret;
+	unsigned int	i;
+	int				cntr;
 
-	ret = 0;
 	i = 0;
+	cntr = 0;
 	while (s[i])
 	{
-		if (s[i] != c && (s[i - 1] == c || i == 0))
-			ret++;
-		i++;
+		while (s[i] == c)
+			i++;
+		if (s[i] != '\0')
+			cntr++;
+		while (s[i] && (s[i] != c))
+			i++;
 	}
-	return (ret);
+	return (cntr);
 }
 
-char	*word_copy(const char *str, char *word, int i, int word_length)
+static int			starter(const char *s, char c, int i)
 {
-	int x;
-	int icpy;
+	while (s[i] == c)
+		i++;
+	return (i);
+}
 
-	icpy = i;
-	x = 0;
-	while (word_length > 0)
+static int			ender(const char *s, char c, int w)
+{
+	while (s[w] && s[w] != c)
+		w++;
+	return (w);
+}
+
+static char			*copyer(const char *s, char *str, int i, int w)
+{
+	int		p;
+
+	p = 0;
+	while (i < w)
 	{
-		word[x] = str[i];
-		word_length--;
-		x++;
+		str[p] = s[i];
 		i++;
+		p++;
 	}
-	word[x] = '\0';
-	return (word);
+	str[p] = '\0';
+	return (str);
 }
-	
 
-char	**ft_strsplit(const char *str, char letter)
+char				**ft_strsplit(char const *s, char c)
 {
-	int i;
-	int x;
-	int w_count = word_count(str, letter);
-	char *word;
 	char **tab;
-	int	word_length;
-	int	icpy;
 
-	if (!(tab = (char **)malloc(sizeof(char *) * w_count + 1)))
+	STEVEN;
+	tab = NULL;
+	if (!s || (!(tab = (char **)malloc(sizeof(char *) * (cnt(s, c)) + 1))))
 		return (NULL);
-	x = 0;
-	i = 0;
-	icpy = 0;
-	word_length = 0;
-	tab[w_count] = 0;
-	while (str[i])
+	tab[cnt(s, c)] = NULL;
+	while (s[i])
 	{
-		while (str[i] == letter)
-			i++;
-		icpy = i;
-		while (str[i] != letter && str[i])
-		{
-			i++;
-			word_length++;
-		}
-		if ((word = (char *)malloc(sizeof(char) * word_length)) == 0)
+		i = starter(s, c, i);
+		w = ender(s, c, i);
+		if (s[i] == '\0')
+			return (tab);
+		str = (char *)malloc(sizeof(*str) * (w - i) + 1);
+		if (str == NULL)
 			return (NULL);
-		tab[x] = word_copy(str, word, icpy, word_length);
-		if (x + 1 == w_count)
-			break ;
-		x++;
-		word_length = 0;
+		str = copyer(s, str, i, w);
+		tab[pos] = str;
+		pos++;
+		i = w - 1;
+		i++;
 	}
 	return (tab);
 }
-		
-/*int		main()
-{
-	char **tab;
-	int x = 0;
-	char str[20] = "Hello World How Are\0";
-	char	c = ' ';
-
-	tab = str_split(str, c);
-	while (tab[x] != 0)
-	{
-		printf("%s", tab[x]);
-		x++;
-	}
-	return (1);
-}
-*/
